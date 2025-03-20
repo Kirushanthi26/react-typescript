6e064a57d9bc240cb97510537f8822047bcbe5ee
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-03-20 12:03:46 +0530

    form reset

--- 2-react_timer/src/components/Form.tsx
+++ 2-react_timer/src/components/Form.tsx
@@ -1,19 +1,22 @@
-import { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import { type FormEvent, type ComponentPropsWithoutRef, useRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
 export const Form = ({ children, onSave, ...otherProps }: FormProps) => {
+  const form = useRef<HTMLFormElement>(null);
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData);
     onSave(data);
+    form.current?.reset();
   };
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
